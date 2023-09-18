b4f0aa2ab0c0f750d09d61dd158c354a3f654705
nsavinda <nirmalsavinda29@gmail.com>
2023-09-18 10:30:52 +0530

    fix:Signup controller

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -4,6 +4,7 @@ class Signup {
     use Controller;
 
     public function index(){
+        $data = [];
 
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $user = new User;
@@ -12,11 +13,12 @@ class Signup {
                 $user->insert($_POST);
                 redirect('login');
             }
+            $data['errors'] = $user->errors;
         }
         
 
         // show($_POST);
-        $data['errors'] = $user->errors;
+       
         $this->view('signup',$data);
     }
 }
