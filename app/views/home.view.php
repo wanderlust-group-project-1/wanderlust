b49ce79db344354c5c6bef86fa837e2c6ab4d739
nsavinda <nirmalsavinda29@gmail.com>
2023-09-26 19:26:29 +0530

    feat:Home page

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,3 +1,26 @@
+<?php
+require_once('../app/views/layout/header.php');
+
+
+?>
+
+<nav class="navbar">
+    <div class="logo">
+        <img src="<?=ROOT_DIR?>/assets/images/logo.png" alt="logo">
+    </div>
+    <ul class="nav-menu">
+        <li class="nav-menu-item"><a href="#">Home</a></li>
+        <li class="nav-menu-item"><a href="#">About</a></li>
+        <li class="nav-menu-item"><a href="#">Services</a></li>
+        <li class="nav-menu-item"><a href="#">Portfolio</a></li>
+        <li class="nav-menu-item"><a href="#">Contact</a></li>
+    </ul>
+    <div class="login-button"><a href="#">Login</a></div>
+</nav>
+
+<?php require_once('../app/views/sections/hero.php');
+ ?>
+
 <h1> Home page view </h1>
 
 
