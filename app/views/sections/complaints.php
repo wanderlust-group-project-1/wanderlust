6f1a54e09c3cabc48d451bf8fc2e4839b4a3116b
SandaliGunawardena <sandaligun@gmail.com>
2023-09-28 15:44:44 +0530

    feat: Complaints section

---
+++ app/views/sections/complaints.php
@@ -0,0 +1,14 @@
+<div class="guide">
+    <div class="home-container row">
+        <div class="col col-2">
+            <h2 class="about-subtitle">Complaints</h2>
+            <h1 class="guide-title">Where Complaints Find Solutions!</h1>
+            <p class="hero-description">Tired of unreliable guides and missing gear? <br>Discover our camping equipment rental and guide hiring platform, where adventure meets satisfaction.</p>
+            <div class="section-button"><a href="#">Complain</a></div>
+        </div>
+        <div class="col col-2">
+            <img class=" locked-aspect-ratio" src=" <?= ROOT_DIR ?>/assets/images/guide.png" alt="Mask group Image">
+        </div>
+    </div>
+
+</div>
---
+++ app/views/sections/tips.php
@@ -0,0 +1,17 @@
+<div class="guide">
+    <div class="home-container row">
+
+        <div class="col col-2">
+            <img src="<?= ROOT_DIR ?>/assets/images/Mask group.png" alt="Hero Image">
+        </div>
+        <div class="col col-4-3-end">
+            <h2 class="about-subtitle">Tips & knowhows</h2>
+            <h1 class="rental-title">Outdoor Knowledge Hub!</h1>
+            <p class="hero-description">Empower your camping journey with expert tips, tutorials, and resources to help enthusiasts make the most of their adventures.</p>
+            <div class="right-align">
+                <div class="section-button "><a href="#">Tips</a></div>
+            </div>
+        </div>
+    </div>
+
+</div>
