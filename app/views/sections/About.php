899793e5d75d594b71898926e00dd236eadf95d0
SandaliGunawardena <sandaligun@gmail.com>
2023-09-28 10:11:31 +0530

    feat:About section

---
+++ app/views/sections/About.php
@@ -0,0 +1,11 @@
+<div class="home-container">
+    <div class="row">
+        <div class="col col-2">
+            <h1 class="about-title">About us!</h1>
+            <p class="about-description">We're your one-stop solution for hassle-free camping adventures. Say goodbye to equipment woes, wasted effort, and unreliable guides. We connect outdoor enthusiasts with quality gear and trustworthy guides.</p>
+        </div>
+        <div class="col col-2">
+            <img src="<?= ROOT_DIR ?>/assets/images/Mask group.png" alt="Mask group Image">
+        </div>
+    </div>
+</div>
--- app/views/sections/hero.php
+++ app/views/sections/hero.php
@@ -1,12 +1,12 @@
 <div class="hero">
-        <div class="home-container row">
-<div class="col col-2">
+    <div class="home-container row">
+        <div class="col col-2">
             <h1 class="hero-title">Campers,</h1>
-            <p class="hero-description">Your Gateway to Outdoor Adventures: <br>
-            Gear Up & Go with Expert Guides!</p>
+            <p class="hero-description"><b>Your Gateway to Outdoor Adventures:</b> <br>
+                Gear Up & Go with Expert Guides!</p>
         </div>
         <div class="col col-2">
-            <img src="<?=ROOT_DIR ?>/assets/images/hero.png" alt="Hero Image">
+            <img src="<?= ROOT_DIR ?>/assets/images/hero.png" alt="Hero Image">
         </div>
-</div>
+    </div>
 </div>
