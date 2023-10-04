a33359365153bb9f3b60c5fc3ca481300f5b01d3
nsavinda <nirmalsavinda29@gmail.com>
2023-10-04 23:46:43 +0530

    feat:use env

--- app/core/init.php
+++ app/core/init.php
@@ -1,15 +1,18 @@
 <?php
 
 
+// use Dotenv\Dotenv;
+// use Firebase\JWT\JWT;
+
 spl_autoload_register(function (string $classname): void {
     $filename = "../app/models/" . trim(ucfirst($classname), 'Model') . ".php";
     require $filename;
 });
 
+require 'functions.php';
 
 require 'config.php';
 require 'declarations.php';
-require 'functions.php';
 require 'Database.php';
 require 'Model.php';
 require 'Controller.php';
