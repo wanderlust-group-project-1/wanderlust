79efd136b0931ece23801c2c80c41774d57c4646
nsavinda <nirmalsavinda29@gmail.com>
2023-10-27 18:09:52 +0530

    feat:user signup

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -13,3 +13,19 @@ load(urlFragment)
 }
 
 console.log(urlFragment);
+
+
+
+
+// File input 
+var fileInput = document.getElementById("verification_document");
+
+var fileLabel = document.querySelector(".file-label");
+
+fileInput.addEventListener("change", function () {
+    if (fileInput.files.length > 0) {
+        fileLabel.textContent = "File Selected: " + fileInput.files[0].name;
+    } else {
+        fileLabel.textContent = "Choose Verification Document";
+    }
+});
