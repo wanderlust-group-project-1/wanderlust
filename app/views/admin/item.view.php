bd5b067d6557d596401fe4670f0be39b40cb00d6
nsavinda <nirmalsavinda29@gmail.com>
2023-10-27 16:23:52 +0530

    feat:admin items

--- app/views/admin/item.view.php
+++ app/views/admin/item.view.php
@@ -1,7 +1,6 @@
 <?php
 require_once('../app/views/admin/layout/header.php');
 require_once('../app/views/admin/layout/sidebar.php');
-
 ?>
 
 <div class="table-container">
@@ -9,28 +8,32 @@ require_once('../app/views/admin/layout/sidebar.php');
         <thead>
             <tr>
                 <th>Name</th>
-                <th>Email</th>
-                <th>Status</th>
+                <th>Type</th>
+                <th>Quantity</th>
+                <th>Price</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>John Doe</td>
-                <td>john@example.com</td>
-                <td><span class="status accepted">Accepted</span></td>
+                <td>Item 1</td>
+                <td>Type A</td>
+                <td>5</td>
+                <td>$10.00</td>
                 <td><button class="view-button">View</button></td>
             </tr>
             <tr>
-                <td>Jane Smith</td>
-                <td>jane@example.com</td>
-                <td><span class="status not-accepted">Not Accepted</span></td>
+                <td>Item 2</td>
+                <td>Type B</td>
+                <td>10</td>
+                <td>$15.50</td>
                 <td><button class="view-button">View</button></td>
             </tr>
             <tr>
-                <td>Bob Johnson</td>
-                <td>bob@example.com</td>
-                <td><span class="status waiting">Waiting</span></td>
+                <td>Item 3</td>
+                <td>Type C</td>
+                <td>3</td>
+                <td>$8.75</td>
                 <td><button class="view-button">View</button></td>
             </tr>
             <!-- Add more rows as needed -->
@@ -38,71 +41,59 @@ require_once('../app/views/admin/layout/sidebar.php');
     </table>
 </div>
 
-<!-- Modal  -->
-<div class="rental-services-modal" id="rental-services-modal">
-<div class="modal-content">
+<!-- Modal -->
+<div class="item-details-modal" id="item-details-modal">
+    <div class="modal-content">
         <span class="close">&times;</span>
-        <div class="profile-info">
-            <img src="<?php echo ROOT_DIR?>/assets/images/dp.jpg" alt="Profile Image" class="profile-image">
-            <h2 id="profile-name">Sandali </h2>
-            <p id="profile-email">sandali@gmail.com</p>
-            <p id="profile-address">Maharagama</p>
-            <p id="profile-status" class="accepted">Accepted</p>
-            <div class="profile-links">
-                <a href="#" id="link-1">Link 1</a>
-                <a href="#" id="link-2">Link 2</a>
-            </div>
+        <div class="item-info">
+            <h2 id="item-name">Item Name</h2>
+            <p id="item-type">Type: Type A</p>
+            <p id="item-quantity">Quantity: 5</p>
+            <p id="item-price">Price: $10.00</p>
         </div>
     </div>
 </div>
 
-
 <script>
+    // Get the modal
+    var modal = document.getElementById("item-details-modal");
 
-// Get the modal
-var modal = document.getElementById("rental-services-modal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get all view buttons
+    var viewButtons = document.querySelectorAll('.view-button');
 
-// Get all view buttons
-var viewButtons = document.querySelectorAll('.view-button');
+    // Function to handle modal display
+    function openModal(content) {
+        // document.getElementById("modal-content").innerHTML = content;
+        modal.style.display = "block";
+    }
 
-// Function to handle modal display
-function openModal(content) {
-    // document.getElementById("modal-content").innerHTML = content;
-    modal.style.display = "block";
-}
-
-// Add click event listener to view buttons
-viewButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        
-        var name = this.parentElement.parentElement.querySelector('td:first-child').textContent;
-        var email = this.parentElement.parentElement.querySelector('td:nth-child(2)').textContent;
-        openModal("Name: " + name + "<br>Email: " + email);
+    // Add click event listener to view buttons
+    viewButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var name = this.parentElement.parentElement.querySelector('td:first-child').textContent;
+            var type = this.parentElement.parentElement.querySelector('td:nth-child(2)').textContent;
+            var quantity = this.parentElement.parentElement.querySelector('td:nth-child(3)').textContent;
+            var price = this.parentElement.parentElement.querySelector('td:nth-child(4)').textContent;
+            openModal("Name: " + name + "<br>Type: " + type + "<br>Quantity: " + quantity + "<br>Price: " + price);
+        });
     });
-});
 
-// Close the modal when the close button is clicked
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target == modal) {
+    // Close the modal when the close button is clicked
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
 
+    // Close the modal if the user clicks outside of it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 </script>
 
-
-
-
-      <?php
+<?php
 require_once('../app/views/admin/layout/footer.php');
-
-
 ?>
