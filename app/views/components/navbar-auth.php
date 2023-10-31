7c1909bdfddd0ce5c60051a83fcf1e7c5001ae69
nsavinda <nirmalsavinda29@gmail.com>
2023-10-31 06:44:32 +0530

    git: fix permission error

---
+++ app/views/components/navbar-auth.php
@@ -0,0 +1,40 @@
+<nav class="navbar">
+    <div class="logo" style="text-align: center;">
+    <img src="<?= ROOT_DIR ?>/assets/images/logo.png" alt="logo" style="display: block; margin: auto;">
+</div>
+
+
+</nav>
+
+<script>
+// Get the profile avatar element and the dropdown menu
+var profileAvatar = document.querySelector('.profile-avatar');
+var dropdownMenu = document.getElementById('nav-dropdown');
+
+// Toggle the dropdown menu when clicking on the profile avatar
+profileAvatar.addEventListener('click', function(event) {
+    // Prevent the default behavior of the anchor tags
+    event.preventDefault();
+
+    // Toggle the display style of the dropdown menu
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+    }
+});
+
+// Close the dropdown menu if the user clicks outside of it
+document.addEventListener('click', function(event) {
+    if (!profileAvatar.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = 'none';
+    }
+});
+
+// Prevent event propagation when clicking on dropdown links
+dropdownMenu.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+
+    </script>
