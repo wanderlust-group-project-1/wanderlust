e0777c5e3d9c69f67cde44ace2734e0971638552
nsavinda <nirmalsavinda29@gmail.com>
2023-11-07 18:35:23 +0530

    test:add test file

--- test.py
+++ test.py
@@ -9,13 +9,13 @@ from time import sleep
 options = webdriver.ChromeOptions()
 
 
-# options.binary_location = '/usr/bin/google-chrome-stable"'
+options.binary_location = '/usr/bin/google-chrome-stable"'
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument("--window-size=1024,768")
 
-chrome_driver_path = "/usr/local/share/chromedriver-linux64"
+chrome_driver_path = "/usr/bin/chromedriver"
 service = Service(chrome_driver_path)
 
 driver = webdriver.Chrome(service=service,options=options)
