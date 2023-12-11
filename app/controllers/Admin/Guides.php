ab895207bec2361dbc75a57ca2b564b11755c5db
nsavinda <nirmalsavinda29@gmail.com>
2023-12-11 20:11:46 +0530

    feat:admin- guide list view

--- app/controllers/Admin/Guides.php
+++ app/controllers/Admin/Guides.php
@@ -13,7 +13,17 @@ class Guides {
 
     public function item(string $a = '', string $b = '', string $c = ''):void {
         $this->view('admin/guides');
-    }}
+    }
+
+    public function viewUser(string $a = '', string $b = '', string $c = ''):void {
+        $guide = new GuideModel();
+        // $data['guide'] = $guide->getGuide($a)[0];
+        // // show(  $data['rental']);
+        // $this->view('admin/guides/user', $data);
+    }
+}
+
+
 
 
 ?>
--- app/views/admin/guides.view.php
+++ app/views/admin/guides.view.php
@@ -33,16 +33,83 @@ require_once('../app/views/admin/layout/sidebar.php');
     </table>
 </div>
 
-<!-- Rest of the code remains unchanged -->
+
+<!-- Modal  -->
+<div class="user-modal" id="guides-modal">
+<div class="modal-content">
+        <span class="close">&times;</span>
+        <div id="user">
+        <div class="profile-info">
+            <img src="<?php echo ROOT_DIR?>/assets/images/dp.jpg" alt="Profile Image" class="profile-image">
+            <h2 id="profile-name">Sandali </h2>
+            <p id="profile-email">sandali@gmail.com</p>
+            <p id="profile-address">Maharagama</p>
+            <p id="profile-status" class="accepted">Accepted</p>
+            <div class="profile-links">
+                <a href="#" id="link-1">Link 1</a>
+                <a href="#" id="link-2">Link 2</a>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+
 
 <script>
-    // Update the modal content with the fetched data
+
+// Get the modal
+var modal = document.getElementById("guides-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get all view buttons
+var viewButtons = document.querySelectorAll('.view-button');
+
+// Function to handle modal display
+function openModal(id) {
+    // document.getElementById("modal-content").innerHTML = content;
+    
+    modal.style.display = "block";
     $.get(`<?php echo ROOT_DIR?>/admin/guides/viewUser/${id}`, function(data) {
-        $("#user").html(data);
+            // Update the modal content with the fetched data
+            $("#user").html(data);
+        });
+    span.onclick = function() {
+    modal.style.display = "none";
+}
+}
+
+// Add click event listener to view buttons
+viewButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        
+      
+        var key = this.parentElement.parentElement.getAttribute('key');
+
+        openModal(key);
     });
-    // ... (rest of the script remains unchanged)
+});
+
+// Close the modal when the close button is clicked
+
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 </script>
 
-<?php
+
+
+
+      <?php
 require_once('../app/views/admin/layout/footer.php');
+
+
 ?>
+
