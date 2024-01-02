406edcd47492c5e358eb017dbccefc8ad6b68a0b
Nirmal Savinda <nirmalsavinda29@gmail.com>
2024-01-02 18:07:38 +0530

    Update app/views/layout/header.php
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -3,7 +3,6 @@
     <head>
         <title>Wanderlust</title>
         <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/style.css">
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/admin-style.css"> -->
         <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/sidebar.css">
 
         <!-- jquery -->
