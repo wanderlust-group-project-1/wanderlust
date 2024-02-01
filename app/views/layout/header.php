f5c834c39cba7d94e577499b8e5bbb0eaff46728
nsavinda <nirmalsavinda29@gmail.com>
2024-02-01 18:17:26 +0530

    style:Rental Dashboard

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -17,24 +17,24 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/AddComplains.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/AddEquipment.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/calendar.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerBlog.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerDashboard.CSS">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerEquipment.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerGuideSearch.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/EditEquipment.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/EquipmentMyBooking.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideBlog.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideBooking.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideDashboard.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideMyBooking.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/MyShop.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/RentalBlog.css">
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/AddComplains.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/AddEquipment.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/calendar.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerBlog.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerDashboard.CSS"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerEquipment.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerGuideSearch.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/EditEquipment.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/EquipmentMyBooking.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideBlog.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideBooking.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideDashboard.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideMyBooking.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/MyShop.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/RentalBlog.css"> -->
         <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/RentalDashboard.css"> -->
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/Tips.css">
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/viewGuide.css">
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/Tips.css"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/viewGuide.css"> -->
         <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/signUp.css"> 
     </head>
 <body>
