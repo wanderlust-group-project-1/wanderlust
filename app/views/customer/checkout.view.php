e74a4f01d64d146fbd3e1536561b6417692bb49e
nsavinda <nirmalsavinda29@gmail.com>
2024-02-09 06:55:21 +0530

    feat:checkout page

--- app/views/customer/checkout.view.php
+++ app/views/customer/checkout.view.php
@@ -1,3 +1,10 @@
+<?php
+require_once('../app/views/layout/header.php');
+
+require_once('../app/views/components/navbar.php');
+
+?>
+
 <div class="container">
     <div class="rent-items">
 Checkout
