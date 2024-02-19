6016ce0ddaaff8791650f56295a91315223eb37d
Sarani <saranihettiarachchi@gmail.com>
2024-02-19 10:39:23 +0530

    signup forms updated

--- app/views/signup.view.php
+++ app/views/signup.view.php
@@ -8,12 +8,6 @@ require_once('../app/views/layout/header.php');
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 
-
-
-
-
-
-
 <div class="signup-container">
     <div class="column">
 
@@ -22,7 +16,7 @@ require_once('../app/views/layout/header.php');
         <p>It's the time to start your journey! Register here.</p>
             <div class="main">
                 <div class="row-btn">
-                    <button class="signup-card" onclick="load('customer')">
+                    <button class="signup-card" onclick="load('customer')" id="customer">
                         <div class="btn-div">
                             <h3>Customer</h3>
                             <h4>Plan your journey now!</h4>
@@ -58,6 +52,189 @@ require_once('../app/views/layout/header.php');
                     <img src="<?php echo ROOT_DIR?>/assets/images/logo.png" alt="overlay-logo" class="overlay-logo2">
                 </div>
         </div>
+    </div>
+
+    <div class="popupForm" id="popupForm">
+        <div class="close-btn" onclick="load()"><i class="fa fa-times" aria-hidden="true"></i></div>
+            <div class="form-common">
+            
+                <h2>Customer Sign Up</h2>
+                <?php if (isset($errors)) : ?>
+                    <div> <?= implode('<br>', $errors) ?> </div>
+                <?php endif; ?>
+                
+                <div class="form-element">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Ex : Alan Mario" required></br>
+                </div>
+                
+                <div class="form-element">
+                    <label for="address">Address</label>
+                    <textarea type="text" class="address-text" name="address" id="address" required> </textarea>
+                </div>
+                
+                <div class="form-element">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" placeholder="example@gmail.com" required>
+                </div>
+                
+                <div class="form-element">
+                    <label for="number">Mobile Number</label>
+                    <input type="text" name="number" id="number" placeholder="+94 (76) XXX XXX" required>
+                </div>
+                
+                <div class="form-element">
+                    <label for="nic">NIC Number</label>
+                    <input type="text" name="nic" id="nic" placeholder="2001XXXXXXXX" required>
+                </div>
+                
+                <div class="form-element">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Password" required>
+                    <i class="fa fa-eye-slash" aria-hidden="true" id="eyeicon"></i>
+                <h4>Please use a strong password. Password minimum length should be least 8 characters long containing at least one uppercase letter, one digit, and one symbol.</h4>
+                </div>
+                
+                <!-- <input  type="submit" name="submit" value="Signup"> -->
+                <div class="form-element-btn">
+                    <button class="btn btn-medium" id="customer-signup" name="submit" value="signup">Sign Up</button>
+                    <button class="btn-cancel btn-medium" id="customer-signup-cancel" name="submit" value="cancel">Cancel</button>
+                </div>
+                <script>
+                    let eyeicon = document.getElementById("eyeicon");
+                    let password = document.getElementById("password");
+
+                    eyeicon.onclick = function() {
+                        if(password.type == "password"){
+                            password.type = "text";
+                            eyeicon.className = "fa fa-eye";
+
+                        }else{
+                            password.type = "password";
+                            eyeicon.className =  "fa fa-eye-slash";
+                        }
+                    }
+                </script>
+                <div id="error-message"></div>
+                
+            
+        </div>
+    </div>
+
+    <!-- <div class="popupForm" id="popupForm">
+            <div class="close-btn" onclick="load()"><i class="fa fa-times" aria-hidden="true"></i></div>
+                <div class="form-common">
+
+                    <h2>Rental Services Sign Up</h2>
+
+                    <?php if (isset($errors)) : ?>
+                        <div><?= implode('<br>', $errors) ?></div>
+                    <?php endif; ?>
+                    
+                    <div class="form-element">
+                        <label for="business_name">Business Name</label>
+                        <input type="text" name="name" id="business_name"> </br>
+                    </div>
+
+                    <div class="form-element">
+                        <label for="address">Address</label>
+                        <textarea class="address-text" name="address" id="address" required> </textarea></br>
+                    </div>
+
+                    <div class="form-element">
+                    <label for="address">Location</label>
+                        <div class="location-button-container"  id="location-button-container">
+                            <input  id="location"  hidden="true"></br>
+                            <button  id="select-location" class="location-button" type="button">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                Select location in the map
+                            </button>
+                        </div>
+                    </div>
+                    
+                    <div class="form-element">
+                        <label for="registration_number">Business Registration Number/NIC</label>
+                        <input type="text" name="regNo" id="registration_number"></br>
+                    </div>
+                    
+                    <div class="form-element">
+                        <label for="mobile_number">Mobile Number</label>
+                        <input type="text" name="mobile" id="number"></br>
+                    </div>
+                    
+                    <div class="form-element">
+                        <label for="email">Email Address</label>
+                        <input type="text" name="email" id="email"></br>
+                    </div>
+                    
+                    
+                    <div class="tooltip-container" class="hoverable-are">
+                        <div class="toolyp-content">
+                            <h5>Upload verification Documents</h5>
+                            <i class="fas fa-question-circle tooltip-trigger"></i>
+                        </div>
+                        
+                        <div class="tooltip">Documents such as business registration certificate or any other document which can verify your business is legitimate.</div>
+                        <div class="file-input-container">
+                            <div class="form-element">
+                            <label for="verification_document" class="file-label">Choose Verification Document</label>
+                            <input type="file" name="verification_document" id="verification_document" class="file-input">
+                            </div>
+                        </div>
+                    </div></br>
+                    
+                    <div class="form-element">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password" required>
+                        <i class="fa fa-eye-slash" aria-hidden="true" id="eyeicon"></i>
+                        <h4>Please use a strong password. Password minimum length should be least 8 characters long containing at least one uppercase letter, one digit, and one symbol.</h4>
+                    </div>
+                    
+                    <div class="form-element-btn">
+                        <button class="btn btn-medium" id="rental-service-signup" name="submit" value="Sign Up" onclick="rentalServiceSignup(event)" > Sign Up </button>
+                        <button class="btn-cancel btn-medium" id="customer-signup-cancel" name="submit" value="cancel">Cancel</button>
+                    </div>
+
+                    <script>
+                    let eyeicon = document.getElementById("eyeicon");
+                    let password = document.getElementById("password");
+
+                    eyeicon.onclick = function() {
+                        if(password.type == "password"){
+                            password.type = "text";
+                            eyeicon.className = "fa fa-eye";
+
+                        }else{
+                            password.type = "password";
+                            eyeicon.className =  "fa fa-eye-slash";
+                        }
+                    }
+                </script>
+
+                    <div id="error-message"></div>
+                </div>
+            </div> -->
+        
+
+    <!-- <script>
+        // document.querySelector("#customer").addEventListener("click", function(){
+        //     document.querySelector(".popupForm").classList.add("active");
+        // });
+        // document.querySelector(".popupForm .btn-cancel").addEventListener("click", function(){
+        //     document.querySelector(".popupForm").classList.remove("active");
+        // });
+        function openPopup() {
+        document.getElementById("popupForm").classList.add("active");
+    }
+
+    function closePopup() {
+        document.getElementById("popupForm").classList.remove("active");
+    }
+
+    document.getElementById("customer").addEventListener("click", openPopup);
+    </script> -->
+    
+</div>
 
         
 
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,8 +1,26 @@
-function load(id){
-    document.getElementById(id).style.display = "flex";
-    document.getElementById('select').style.display = "none";
+// function load(id){
+//     document.getElementById(id).style.display = "flex";
+//     document.getElementById('select').style.display = "none";
    
+// }
+
+function load(formId) {
+    var form = document.getElementById(formId + '-form');
+    // Rest of the function logic...
 }
+
+
+//showpassowrd
+// let eyeicon = document.getElementById("eyeicon");
+// let password = document.getElementById("password");
+
+// eyeicon.onclick = function() {
+//     if(password.type == "password"){
+//         password.type = "text";
+//     }else{
+//         password.type = "password";
+//     }
+// }
 
 // Get Url
 var urlFragment = window.location.hash;
