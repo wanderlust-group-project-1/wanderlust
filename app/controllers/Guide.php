7d93af508d999106d133a9906ec09fb5cfc153fd
Gayandee <2021cs153@stu.ucsc.cmb.ac.lk>
2024-04-05 12:35:35 +0530

    Edits in guide

--- app/controllers/Guide.php
+++ app/controllers/Guide.php
@@ -13,12 +13,25 @@ class Guide {
 
     public function update(string $a = '', string $b = '', string $c = ''): void
     {
-
         $guide = new guideModel();
         $guide->updateGuide($_POST);
-
+    
+        // Determine the current page URL
+        $currentPage = rtrim($_SERVER['REQUEST_URI'], '/');
+    
+        // Check if it's the dashboard page
+        if ($currentPage === ROOT_DIR . '/dashboard') {
+            redirect('dashboard');
+        }
+        // Check if it's the packages page
+        elseif ($currentPage === ROOT_DIR . '/packages') {
+            redirect('packages');
+        }
+        // If it's neither dashboard nor packages, you can add more conditions as needed
+    
+        // Default redirect
         redirect('dashboard');
         // $this->view('customer/profile');
-    }
+    }    
 
 }
--- app/views/guide/packages.view.php
+++ app/views/guide/packages.view.php
@@ -2,96 +2,200 @@
 require_once('../app/views/layout/header.php');
 ?>
 
-<!-- search bar -->
-
-<!-- <div class="package-card flex-d-r">
-    <div class="pack-img">
-
-    </div>
-    <div class="pack-set flex-d-r">
-        <div>
-            <h1 class="title"> Platinum <h1>
-        </div>
-        <div class="pack-benefits .flex-d-r">
-            <div>
-                <p></p> 
-            </div>
-            <div>
-                <p></p> 
-            </div>
-        </div>
-        <div class="pack-benefits">
-            <div>
-                <p></p> 
-            </div>
-            <div>
-                <p></p> 
-            </div>
-        </div>
-    </div>
-</div> -->
-
-
 <div class="dashboard">
     <?php require_once('../app/views/guide/layout/guide-sidebar.php');
     ?>
 
     <div class="sidebar-flow"></div>
 
-    <div class="guide-dash-main">
+    <div class="guide-dash-main flex-d-c">
         <h1 class="title mb-2">My Packages</h1>
         <ul class="breadcrumbs">
             <li><a href="<?= ROOT_DIR ?>/home">Home</a></li>
             <li class="divider">/</li>
             <li><a href="#" class="active">My Packages</a></li>
         </ul>
-        <div class="info-data mt-5">
-            <div class="card">
+
+        <div class="data">
+            <div class="content-data">
                 <div class="head">
-                    <div>
-                        <h2>96</h2>
-                        <p>No of Tours</p>
+                    <h3>Package 01</h3>
+                    <h2>Rs.10 000</h2>
+
+                </div>
+
+                <div class="info-data mt-5">
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>10</h2>
+                                <p>Maximum Group Size</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>20km</h2>
+                                <p>Maximum Distance</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>Yes</h2>
+                                <p>Transport Needed</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>Nuwara Eliya, Ella</h2>
+                                <p>Places</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <span class="progress" data-value="12.5%"></span>
-                <span class="label">12 : Per Month</span>
+                <div class="card rounded-4">
+                    <button type="submit" class="btn-edit rounded-6 edit-package">
+                        Edit Package
+                    </button>
+                </div>
             </div>
-            <div class="card">
+        </div>
+
+        <div class="data">
+            <div class="content-data">
                 <div class="head">
-                    <div>
-                        <h2>Rs.139000</h2>
-                        <p>Income</p>
+                    <h3>Package 02</h3>
+                    <h2>Rs.20 000</h2>
+
+                </div>
+
+                <div class="info-data mt-5">
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>20</h2>
+                                <p>Maximum Group Size</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>30km</h2>
+                                <p>Max Distance</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>No</h2>
+                                <p>Transport Needed</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="head">
+                            <div>
+                                <h2>Nuwara Eliya, Ella</h2>
+                                <p>Places</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <span class="progress" data-value="60%"></span>
-                <span class="label">Rs.60 000 : Per Month</span>
+                <div class="card rounded-4">
+                    <button type="submit" class="btn-edit rounded-6 edit-package">
+                        Edit Package
+                    </button>
+                </div>
             </div>
-            <div class="card">
-                <div class="head">
-                    <div>
-                        <h2>1st March</h2>
-                        <p>Upcoming Booking</p>
-                    </div>
-                </div>
-                <div class="booking-bar .flex-d mt-4 mb-2">
-                    <p>Micheal Julius</p>
-                </div>
-                <!-- <span class="progress" data-value="30%"></span>
-                <span class="label">30%</span> -->
-            </div>
-            <div class="card">
-                <div class="head">
-                    <div>
-                        <h2>21st February</h2>
-                        <p>Recent Booking</p>
-                    </div>
-                </div>
-                <div class="booking-bar .flex-d mt-4 mb-2 ml-2">
-                    <p>Julius John</p>
-                </div>
-                <!-- <span class="progress" data-value="80%"></span>
-                <span class="label">80%</span> -->
-            </div>
+        </div>
+
+        <div class="flex-d-r-end">
+            <button type="submit" class="btn mt-4" id="edit-profile">
+                        Add New Package
+            </button>
         </div>
     </div>
 </div>
+
+
+<!-- Modal Box Pacakge Edit -->
+<div class="package-editor" id="package-editor" style="display: block;">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <div class="package-info">
+      <img src="<?php echo ROOT_DIR ?>/assets/images/2.png" alt="Profile Image" class="profile-image">
+
+      <form id="packages" action="<?= ROOT_DIR ?>/package/update/<?= $package['id'] ?>" method="post">
+          <h2>Update Package Details</h2>
+          <?php if (isset($errors)) : ?>
+            <div> <?= implode('<br>', $errors) ?> </div>
+          <?php endif; ?>
+
+        <label for="price">Price</label>
+        <input type="text" name="price" id="price" value="<?= $package['price'] ?>" required>
+
+        <label for="max_group_size">Maximum Group Size</label>
+        <input type="text" name="max_group_size" id="max_group_size" value="<?= $package['max_group_size'] ?>" required>
+
+        <label for="max_distance">Maximum Distance</label>
+        <input type="text" name="max_distance" id="max_distance" value="<?= $package['max_distance'] ?>" required>
+
+        <label for="transport_needed">Transport Needed</label>
+        <select name="transport_needed" id="transport_needed" required>
+            <option value="1" <?= !$package['transport_needed'] ? 'selected' : '' ?>>Yes</option>
+            <option value="0" <?= !$package['transport_needed'] ? 'selected' : '' ?>>No</option>
+        </select>
+
+        <label for="places">Places</label>
+        <input type="text" name="places" id="places" value="<?= $package['places'] ?>" required>
+
+        <input type="submit" class="btn mt-4" name="submit" value="Update">    
+      </form>
+
+    <!-- <span class="close">&times;</span> -->
+    </div>
+</div>
+<!-- Modal Box Package Edit End -->
+
+<script>
+  var modal = document.getElementById("package-editor");
+
+  var span = document.getElementsByClassName("close")[0];
+
+  // Get all view buttons
+  var viewButton = document.querySelector('.edit-package');
+
+  // Function to handle modal display
+  function openModal() {
+    // document.getElementById("modal-content").innerHTML = content;
+    modal.style.display = "block";
+  }
+
+  // Add click event listener to view buttons
+  viewButton.addEventListener('click', function() {
+
+    // var name = this.parentElement.parentElement.querySelector('td:first-child').textContent;
+    // var email = this.parentElement.parentElement.querySelector('td:nth-child(2)').textContent;
+    openModal();
+  });
+
+
+  // Close the modal when the close button is clicked
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // Close the modal if the user clicks outside of it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+</script>
