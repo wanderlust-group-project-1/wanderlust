b094bea52c2e05912318ca2a4486c3a1ac7f2d47
SandaliGunawardena <sandaligun@gmail.com>
2024-04-17 10:04:31 +0530

    admin_stats

--- app/views/admin/chart.view.php
+++ app/views/admin/chart.view.php
@@ -1,40 +1,55 @@
 <?php
-require_once('../app/views/admin/layout/header.php');
+require_once('../app/views/layout/header.php');
 require_once('../app/views/admin/components/navbar.php');
 
-require_once('../app/views/admin/layout/sidebar.php');
+// require_once('../app/views/admin/layout/sidebar.php');
+
 ?>
 
-<div class="table-container">
-  <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
-</div>
+<div class="dashboard">
+  <?php require_once('../app/views/admin/layout/sidebar.php');
+  ?>
 
-<script>
-  var xValues = ["Rental Services", "Guides", "Registered Customers"];
-  var yValues = [192, 89, 369];
-  var barColors = ["green", "blue", "orange"];
+  <div class="sidebar-flow"></div>
 
-  new Chart("myChart", {
-    type: "bar",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
-    },
-    options: {
-      legend: {
-        display: false
+
+  <!-- <?php
+        require_once('../app/views/admin/layout/header.php');
+        require_once('../app/views/admin/components/navbar.php');
+
+        require_once('../app/views/admin/layout/sidebar.php');
+        ?> -->
+
+  <div class="table-container">
+    <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+  </div>
+
+  <script>
+    var xValues = ["Rental Services", "Guides", "Customers"];
+    var yValues = [15, 27, 32];
+    var barColors = ["#8D9E6F", "#526534", "#8D9E6F"];
+
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: xValues,
+        datasets: [{
+          backgroundColor: barColors,
+          data: yValues
+        }]
       },
-      title: {
-        display: true,
-        text: "Wanderlust Users - 2023"
+      options: {
+        legend: {
+          display: false
+        },
+        title: {
+          display: true,
+          text: "Wanderlust Users"
+        }
       }
-    }
-  });
-</script>
+    });
+  </script>
 
-<?php
-require_once('../app/views/admin/layout/footer.php');
-?>
+  <?php
+  require_once('../app/views/admin/layout/footer.php');
+  ?>
--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -1,42 +1,43 @@
-
 <html>
-    <head>
+
+<head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Wanderlust</title>
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/style.css">
+    <title>Wanderlust</title>
+    <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/style.css">
 
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/admin-style.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/admin-style.css"> -->
 
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/scss/wl/dashboard/index.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/scss/wl/dashboard/index.css">
 
-        <script src="<?= ROOT_DIR ?>/assets/js/script.js"></script>
-
-
-
-        <!-- jquery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="<?= ROOT_DIR ?>/assets/js/script.js"></script>
 
 
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/AddComplains.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/AddEquipment.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/calendar.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerBlog.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerDashboard.CSS"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerEquipment.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/CustomerGuideSearch.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/EditEquipment.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/EquipmentMyBooking.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideBlog.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideBooking.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideDashboard.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/GuideMyBooking.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/MyShop.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/RentalBlog.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/RentalDashboard.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/Tips.css"> -->
-        <!-- <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/viewGuide.css"> -->
-        <link rel="stylesheet" type="text/css" href="<?=ROOT_DIR ?>/assets/css/signUp.css"> 
-    </head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> -->
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/AddComplains.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/AddEquipment.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/calendar.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/CustomerBlog.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/CustomerDashboard.CSS"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/CustomerEquipment.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/CustomerGuideSearch.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/EditEquipment.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/EquipmentMyBooking.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/GuideBlog.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/GuideBooking.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/GuideDashboard.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/GuideMyBooking.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/MyShop.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/RentalBlog.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/RentalDashboard.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/Tips.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/viewGuide.css"> -->
+    <link rel="stylesheet" type="text/css" href="<?= ROOT_DIR ?>/assets/css/signUp.css">
+</head>
+
 <body>
-
