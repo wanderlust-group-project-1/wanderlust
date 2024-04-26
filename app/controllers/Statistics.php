835837364e9cc3ad7a18a55f2adab253d94bca9e
nsavinda <nirmalsavinda29@gmail.com>
2024-04-26 09:15:01 +0530

    fix:remove unwanted cards rental statistics

--- app/controllers/Statistics.php
+++ app/controllers/Statistics.php
@@ -9,10 +9,17 @@ class Statistics
     {
 
         AuthorizationMiddleware::authorize(['rentalservice']);
+        $user = UserMiddleware::getUser();
+
 
         $rental = new RentalServiceModel;
+        $rent = new RentModel;
+
         $data = [
-            'stat' => $rental->rentalStats(UserMiddleware::getUser()['id'])[0]
+            'stat' => $rental->rentalStats(UserMiddleware::getUser()['id'])[0],
+            'rent' => $rent->getUpcomingRentByRentalService(['rentalservice_id' => $user['id']])
+
+
         ];
 
 
