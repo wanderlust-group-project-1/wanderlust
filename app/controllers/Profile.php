3caa58eac4a3a7bf53ed1d3cd927e43a251599d6
nsavinda <nirmalsavinda29@gmail.com>
2024-04-29 12:13:48 +0530

    fix

--- app/controllers/Profile.php
+++ app/controllers/Profile.php
@@ -18,9 +18,17 @@ class Profile {
         if ($user['role']== 'customer') {
 
         $rent = new RentModel;
+        $guideBooking = new GuideBookingsModel;
+        $complaint = new RentComplaintModel;
         $data = [ 
             'rental' => $rent->getUpcomingRentByCustomer(['customer_id' => $user['id']])[0],
+            'ordersCount' => $rent->count(['customer_id' => $user['id'], 'status' => 'completed']) + $rent->count(['customer_id' => $user['id'], 'status' => 'rented']),
+            'guideBookingsCount' => $guideBooking->count(['customer_id' => $user['id'], 'status' => 'completed']),
+            'complaintCount' => sizeof($complaint->getComplaintsByCustomer(['customer_id' => $user['id']]))
+
         ];
+
+        // show($data);
 
 
             $this->view('customer/profile', $data);
