cab7ebcfd24f9e464eb89e8d90bdb9299b44281c
nsavinda <nirmalsavinda29@gmail.com>
2024-04-30 07:27:16 +0530

    fix:Authorization guide

--- app/controllers/GuideBookings.php
+++ app/controllers/GuideBookings.php
@@ -3,6 +3,7 @@
 class GuideBookings{
     use Controller;
     public function index(string $a = '', string $b = '', string $c = ''): void {
+        AuthorizationMiddleware::authorize(['guide']);
         $this->view('guide/guidebooking');
     }
 }
--- app/controllers/Guidestatistics.php
+++ app/controllers/Guidestatistics.php
@@ -4,6 +4,7 @@ class Guidestatistics {
     use Controller;
 
     public function index(string $a = '', string $b = '', string $c = ''): void {
+        AuthorizationMiddleware::authorize(['guide']);
         $this->view('guide/guideStatistics');
     }
 
